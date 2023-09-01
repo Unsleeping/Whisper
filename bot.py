480b9936bdcccd168b130e04e135aec51fa4805d
Alexander Mandrov <otvet-1995@mail.ru>
2023-09-01 10:50:52 +0300

    support large file recognition by providing Google Drive link to the large file (large is to be more than 20 Mb), some except catching, processing messages more clearly, refactoring

--- bot.py
+++ bot.py
@@ -1,18 +1,29 @@
-import telebot
 import config
 import utils
 import random
-
+import gdown
+from telebot.apihelper import ApiTelegramException
 from telebot import types
+from utils import bot
 
-bot = telebot.TeleBot(config.token)
+
+def start_worker(callback, fallback):
+    if utils.queued:
+        fallback()
+
+    utils.queued = True
+    callback()
+    utils.queued = False
 
 
 @bot.message_handler(commands=['start', 'Start'])
 def start_message(message):
+    keyboard1 = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
     keyboard2 = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
-    button_language = types.KeyboardButton(text="/recognise")
-    keyboard2.add(button_language)
+    button_start = types.KeyboardButton(text="/start")
+    button_recognise = types.KeyboardButton(text="/recognise")
+    keyboard1.add(button_start)
+    keyboard2.add(button_recognise)
     bot.send_message(message.chat.id,
                      utils.get_introducing(message.from_user.first_name, bot.get_me().first_name),
                      parse_mode='html',
@@ -26,27 +37,58 @@ def send_sticker(message):
     print(message.sticker.file_id)
 
 
-@bot.message_handler(content_types=['audio'])
-def addfile(message):
+@bot.message_handler(content_types=['text'])
+def get_link(message):
+    print(message.text)
     chat_id = utils.get_chat_id(message)
-    if utils.queued:
-        bot.send_message(chat_id, config.get_queued_message())
-        bot.send_sticker(chat_id, random.choice(config.queued_stickers))
-        return None
 
-    utils.queued = True
-    bot.send_message(chat_id, config.get_init_message(), parse_mode='html')
-    bot.send_sticker(chat_id, random.choice(config.loading_stickers))
+    def worker():
+        url = message.text
+        if utils.is_gdrive_link(url):
+            bot.send_message(chat_id, config.get_init_gdrive_message(), parse_mode='html')
+            bot.send_sticker(chat_id, random.choice(config.loading_stickers))
 
-    audio = message.audio
-    file_info = bot.get_file(audio.file_id)
-    downloaded_file = bot.download_file(file_info.file_path)
+            downloadable_url = utils.get_downloadable_url(url)
+            file_name = gdown.download(downloadable_url, config.output_gdrive_file_name, quiet=False)
 
-    [output, file_to_remove] = utils.start_recognition(audio, downloaded_file)
+            bot.send_message(chat_id, config.get_download_complete_gdrive_message(), parse_mode='html')
 
-    bot.send_document(chat_id, output)
-    utils.queued = False
-    utils.remove(file_to_remove, '>> remove recognised text {0}'.format(file_to_remove))
+            [output, file_to_remove] = utils.recognise_from_gdrive(file_name, utils.reply_to_user(chat_id))
+
+            utils.send_result(chat_id, output, file_to_remove)
+
+        else:
+            bot.send_message(chat_id, config.get_error_message_no_gdrive_link(), parse_mode='html')
+
+    start_worker(worker, lambda: utils.queued_fallback(chat_id))
+
+
+@bot.message_handler(content_types=['audio'])
+def add_audio(message):
+    chat_id = utils.get_chat_id(message)
+
+    def worker():
+        try:
+            audio = message.audio
+            file_info = bot.get_file(audio.file_id)
+            downloaded_file = bot.download_file(file_info.file_path)
+
+            bot.send_message(chat_id, config.get_init_message(), parse_mode='html')
+            bot.send_sticker(chat_id, random.choice(config.loading_stickers))
+
+            [output, file_to_remove] = utils.recognise_from_file(audio, downloaded_file, utils.reply_to_user(chat_id))
+
+            utils.send_result(chat_id, output, file_to_remove)
+
+        except ApiTelegramException as e:
+            error_message = config.get_error_message()
+            if utils.is_file_size_limit_exceeded(e):
+                error_message = config.get_error_message_file_size_exceeded()
+
+            bot.send_message(chat_id, error_message, parse_mode='html')
+            bot.send_sticker(chat_id, random.choice(config.error_stickers))
+
+    start_worker(worker, lambda: utils.queued_fallback(chat_id))
 
 
 if __name__ == '__main__':
--- utils.py
+++ utils.py
@@ -1,26 +1,70 @@
 import config
 import subprocess
 import os
+import mimetypes
+import re
+import telebot
+import random
+
+bot = telebot.TeleBot(config.token)
+
+
+def reply_to_user(chat_id):
+    return lambda log: bot.send_message(chat_id, log)
+
+
+def send_result(chat_id, output, file_to_remove):
+    bot.send_document(chat_id, output)
+    remove(file_to_remove, f'>> remove recognised text {file_to_remove}')
+
+
+def queued_fallback(chat_id):
+    bot.send_message(chat_id, config.get_queued_message())
+    bot.send_sticker(chat_id, random.choice(config.queued_stickers))
 
 
 def get_chat_id(message):
     return message.chat.id
 
 
+def is_gdrive_link(url):
+    return "https://drive.google.com/" in url
+
+
+def is_file_size_limit_exceeded(error):
+    return "file is too big" in str(error)
+
+
+def get_downloadable_url(url):
+    return re.sub(r"https://drive\.google\.com/file/d/(.*?)/.*?\?usp=sharing",
+                  r"https://drive.google.com/uc?id=\1", url)
+
+
 def get_introducing(user_name, bot_name):
-    return "Hello {0}! I am - <b>{1}</b>.".format(user_name, bot_name)
+    return f"Hello {user_name}! I am - <b>{bot_name}</b>."
 
 
-def run_speech_recognition(file_name):
-    print('>> start speech recognition', file_name)
+def set_logger(reply):
+    def log(message, is_console=True, is_user=False):
+        if is_console:
+            print(f'>> {message}')
+        if is_user:
+            reply(message)
+
+    return log
+
+
+def run_speech_recognition(file_name, reply):
+    log = set_logger(reply)
+    log('I started speech recognition', is_user=True)
     subprocess.call(config.get_whisper_args(file_name))
-    print('>> finish speech recognition', file_name)
+    log('I finished speech recognition', is_user=True)
     remove(file_name, '>> remove source audio')
 
     output_name = file_name + txt_extension_string
     if os.path.exists(output_name):
         text = open(output_name)
-        print('>> sending recognised text', output_name)
+        log('Sending recognised text', is_user=True)
         return [text, output_name]
 
 
@@ -31,23 +75,32 @@ def remove(file_name, reason=''):
         os.remove(file_name)
 
 
-def start_recognition(audio, downloaded_file):
-    file_name = audio.file_name
-    is_wav = audio.mime_type == 'audio/x-wav'
-    with open(file_name, 'wb') as new_file:
-        new_file.write(downloaded_file)
+def recognise(mime_type, file_name, reply):
+    log = set_logger(reply)
+    is_wav = mime_type == 'audio/x-wav'
     if is_wav:
-        print('>> uploaded file is WAV')
-        return run_speech_recognition(new_file.name)
+        log('uploaded file is WAV')
+        return run_speech_recognition(file_name, reply)
     else:
-        print('>> uploaded file isn\'t WAV')
-        print('>> converting {0.name} into WAV'.format(new_file))
-        subprocess.call(config.get_ffmpeg_args(new_file.name, config.output_file_name))
-        print('>> finish converting {0.name} into WAV'.format(new_file))
-        remove(new_file.name, '>> remove original audio')
-        return run_speech_recognition(config.output_file_name)
+        log('Provided file needs to be converted first into WAV before recognition, I\'m working on it now',
+            is_user=True)
+        subprocess.call(config.get_ffmpeg_args(file_name, config.output_file_name))
+        log('Conversion is finished', is_user=True)
+        remove(file_name, '>> remove original audio')
+        return run_speech_recognition(config.output_file_name, reply)
+
+
+def recognise_from_gdrive(name, reply):
+    return recognise(mimetypes.guess_type(name)[0], name, reply)
+
+
+def recognise_from_file(audio, file, reply):
+    file_name = audio.file_name
+    with open(file_name, 'wb') as new_file:
+        new_file.write(file)
+
+    return recognise(audio.mime_type, new_file.name, reply)
 
 
 queued = False
 txt_extension_string = '.txt'
-
