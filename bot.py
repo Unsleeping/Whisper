b7ae5d0ccede747d7ea2aa8aa3508d9e4d1cbca2
maksim <kolievmax@gmail.com>
2023-10-14 21:56:09 +0300

    run pre-commit

--- bot.py
+++ bot.py
@@ -1,12 +1,24 @@
-import utils
 import random
+
 import gdown
-from telebot.apihelper import ApiTelegramException
 from telebot import types
-from utils import (bot, send_result, get_introducing, get_chat_id, get_logger, reply_to_user, is_gdrive_link,
-                   queued_fallback, is_file_size_limit_exceeded, get_downloadable_url)
+from telebot.apihelper import ApiTelegramException
+
+import utils
 from main import recognise_from_file, recognise_from_gdrive
 from uns_config import *
+from utils import (
+    bot,
+    get_chat_id,
+    get_downloadable_url,
+    get_introducing,
+    get_logger,
+    is_file_size_limit_exceeded,
+    is_gdrive_link,
+    queued_fallback,
+    reply_to_user,
+    send_result,
+)
 
 
 def start_worker(callback, fallback):
@@ -18,7 +30,7 @@ def start_worker(callback, fallback):
     utils.queued = False
 
 
-@bot.message_handler(commands=['start', 'Start'])
+@bot.message_handler(commands=["start", "Start"])
 def start_message(message):
     keyboard1 = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
     keyboard2 = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
@@ -26,20 +38,21 @@ def start_message(message):
     button_recognise = types.KeyboardButton(text="/recognise")
     keyboard1.add(button_start)
     keyboard2.add(button_recognise)
-    bot.send_message(message.chat.id,
-                     get_introducing(message.from_user.first_name, bot.get_me().first_name),
-                     parse_mode='html',
-                     reply_markup=keyboard2)
-    bot.send_message(message.chat.id,
-                     get_started_message())
+    bot.send_message(
+        message.chat.id,
+        get_introducing(message.from_user.first_name, bot.get_me().first_name),
+        parse_mode="html",
+        reply_markup=keyboard2,
+    )
+    bot.send_message(message.chat.id, get_started_message())
 
 
-@bot.message_handler(content_types=['sticker'])
+@bot.message_handler(content_types=["sticker"])
 def send_sticker(message):
     print(message.sticker.file_id)
 
 
-@bot.message_handler(content_types=['text'])
+@bot.message_handler(content_types=["text"])
 def get_link(message):
     print(message.text)
     chat_id = get_chat_id(message)
@@ -48,24 +61,24 @@ def get_link(message):
     def worker():
         url = message.text
         if is_gdrive_link(url):
-            bot.send_message(chat_id, get_init_gdrive_message(), parse_mode='html')
+            bot.send_message(chat_id, get_init_gdrive_message(), parse_mode="html")
             bot.send_sticker(chat_id, random.choice(loading_stickers))
 
             downloadable_url = get_downloadable_url(url)
             file_name = gdown.download(downloadable_url, output_gdrive_file_name, quiet=False)
 
-            bot.send_message(chat_id, get_download_complete_gdrive_message(), parse_mode='html')
+            bot.send_message(chat_id, get_download_complete_gdrive_message(), parse_mode="html")
 
             recognise_from_gdrive(file_name, log)
             send_result(chat_id, log)
 
         else:
-            bot.send_message(chat_id, get_error_message_no_gdrive_link(), parse_mode='html')
+            bot.send_message(chat_id, get_error_message_no_gdrive_link(), parse_mode="html")
 
     start_worker(worker, lambda: queued_fallback(chat_id))
 
 
-@bot.message_handler(content_types=['audio'])
+@bot.message_handler(content_types=["audio"])
 def add_audio(message):
     chat_id = get_chat_id(message)
     log = get_logger(reply_to_user(chat_id))
@@ -76,7 +89,7 @@ def add_audio(message):
             file_info = bot.get_file(audio.file_id)
             downloaded_file = bot.download_file(file_info.file_path)
 
-            bot.send_message(chat_id, get_init_message(), parse_mode='html')
+            bot.send_message(chat_id, get_init_message(), parse_mode="html")
             bot.send_sticker(chat_id, random.choice(loading_stickers))
 
             recognise_from_file(audio, downloaded_file, log)
@@ -87,11 +100,11 @@ def add_audio(message):
             if is_file_size_limit_exceeded(e):
                 error_message = get_error_message_file_size_exceeded()
 
-            bot.send_message(chat_id, error_message, parse_mode='html')
+            bot.send_message(chat_id, error_message, parse_mode="html")
             bot.send_sticker(chat_id, random.choice(error_stickers))
 
     start_worker(worker, lambda: queued_fallback(chat_id))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     bot.polling(none_stop=True)
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import mimetypes
 import subprocess
-from utils import remove, get_sample_rate
+
+from utils import get_sample_rate, remove
 from worker import *
 
 
@@ -13,7 +14,7 @@ def check_rate(file_name, log):
     if current_sample_rate != target_sample_rate:
         log(f"Converting WAV file from {current_sample_rate}Hz to {target_sample_rate}Hz...", is_user=True)
         convert_to_16khz(file_name, converted_file_name)
-        remove(file_name, 'remove original audio')
+        remove(file_name, "remove original audio")
         return converted_file_name
     else:
         log(f"WAV file is already at {target_sample_rate}Hz.")
@@ -22,34 +23,35 @@ def check_rate(file_name, log):
 
 def convert_to_wav(file_name):
     subprocess.call(get_ffmpeg_args_to_wav(file_name, wav_file_name))
-    remove(file_name, 'remove original audio')
+    remove(file_name, "remove original audio")
     return wav_file_name
 
 
 def run_speech_recognition(file_name, log):
-    log('I started speech recognition', is_user=True)
+    log("I started speech recognition", is_user=True)
     subprocess.call(get_whisper_args(file_name))
-    log('I finished speech recognition', is_user=True)
-    remove(file_name, 'remove source audio')
+    log("I finished speech recognition", is_user=True)
+    remove(file_name, "remove source audio")
 
 
 def recognise(mime_type, file_name, log):
-    is_wav = mime_type == 'audio/x-wav'
+    is_wav = mime_type == "audio/x-wav"
     if is_wav:
-        log('Uploaded file is WAV')
+        log("Uploaded file is WAV")
         file_to_proceed = check_rate(file_name, log)
     else:
-        log('Provided file needs to be converted first into WAV before recognition, I\'m working on it now',
-            is_user=True)
+        log(
+            "Provided file needs to be converted first into WAV before recognition, I'm working on it now", is_user=True
+        )
         file_to_proceed = convert_to_wav(file_name)
-        log('Conversion is finished', is_user=True)
+        log("Conversion is finished", is_user=True)
 
     return run_speech_recognition(file_to_proceed, log)
 
 
 def recognise_from_file(audio, file, log):
     file_name = audio.file_name
-    with open(file_name, 'wb') as new_file:
+    with open(file_name, "wb") as new_file:
         new_file.write(file)
     return recognise(audio.mime_type, new_file.name, log)
 
